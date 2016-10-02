a59847015c27d08fae5fbb66d9919bcee56398a5
BANG\ngobang <ngotribang@gmail.com>
2016-10-02 16:34:51 +0700

    Add Web Api

---
+++ TeduShop.Service/ErrorService.cs
@@ -0,0 +1,35 @@
+﻿using TeduShop.Data.Infrastructure;
+using TeduShop.Data.Repositories;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Service
+{
+    public interface IErrorService
+    {
+        Error Create(Error postCategory);
+
+        void Save();
+    }
+
+    public class ErrorService : IErrorService
+    {
+        private IErrorRepository _errorRepository;
+        private IUnitOfWork _unitOfWork;
+
+        public ErrorService(IErrorRepository errorRepository, IUnitOfWork unitOfWork)
+        {
+            _errorRepository = errorRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public Error Create(Error postCategory)
+        {
+            return _errorRepository.Add(postCategory);
+        }
+
+        public void Save()
+        {
+            _unitOfWork.Commit();
+        }
+    }
+}
