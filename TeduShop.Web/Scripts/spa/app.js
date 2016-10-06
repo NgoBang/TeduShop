933427b47c7d6f6016a73ca23e0c7392a22739dd
BANG\ngobang <ngotribang@gmail.com>
2016-10-06 09:54:33 +0700

    $scope

---
+++ TeduShop.Web/Scripts/spa/app.js
@@ -0,0 +1,17 @@
+﻿/// <reference path="../plugins/angular/angular.js" />
+var myApp = angular.module("myModule", []);
+myApp.controller("schoolController", schoolController);
+myApp.controller("studentController", studentController);
+myApp.controller("teacherController", teacherController);
+
+function schoolController($scope) {
+    $scope.message = 'Announcement from school';
+}
+//myController.$inject = ['$scope'];
+//declare
+function studentController($scope) {
+    $scope.message = "This is my message from Student";
+}
+function teacherController($scope) {
+    $scope.message = "This is my message from Teacher";
+}
