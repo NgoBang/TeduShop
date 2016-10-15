69f1e2aa7c44f47914c9645650df58470b40a587
BANG\ngobang <ngotribang@gmail.com>
2016-10-15 13:36:33 +0700

    delete multi records

--- TeduShop.Web/app/shared/modules/tedushop.common.js
+++ TeduShop.Web/app/shared/modules/tedushop.common.js
@@ -1,4 +1,4 @@
 ﻿/// <reference path="/Assets/admin/libs/angular/angular.js" />
 (function () {
-    angular.module('tedushop.common', ['ui.router'])
+    angular.module('tedushop.common', ['ui.router', 'ngBootbox'])
 })();
