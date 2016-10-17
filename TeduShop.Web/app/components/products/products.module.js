c67b01e5f5e2ba6cbd2576cfc2994ea7d9ae7985
BANG\ngobang <ngotribang@gmail.com>
2016-10-17 13:49:08 +0700

    add login form and implement nested template

--- TeduShop.Web/app/components/products/products.module.js
+++ TeduShop.Web/app/components/products/products.module.js
@@ -4,20 +4,24 @@
     angular.module('tedushop.products', ['tedushop.common']).config(config);
     config.$inject = ['$stateProvider', '$urlRouterProvider'];
     function config($stateProvider, $urlRouterProvider) {
-        $stateProvider.state('products', {
-            url: "/products",
-            templateUrl: "/app/components/products/productListView.html",
-            controller: "productListController"
-        })
+        $stateProvider
+            .state('products', {
+                url: "/products",
+                parent: 'base',
+                templateUrl: "/app/components/products/productListView.html",
+                controller: "productListController"
+            })
             .state('add_product', {
-            url: "/add_product",
-            templateUrl: "/app/components/products/productAddView.html",
-            controller: "productAddController"
+                url: "/add_product",
+                parent: 'base',
+                templateUrl: "/app/components/products/productAddView.html",
+                controller: "productAddController"
             })
             .state('edit_product', {
-            url: "/edit_product/:id",
-            templateUrl: "/app/components/products/productEditView.html",
-            controller: "productEditController"
-        });
+                url: "/edit_product/:id",
+                parent: 'base',
+                templateUrl: "/app/components/products/productEditView.html",
+                controller: "productEditController"
+            });
     }
 })();
